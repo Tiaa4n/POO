ded409c52e4e077b5b1f6fe3605fa8036c01dfd3
Tiaa4n <sebastian.ventesar@campusucc.edu.co>
2025-08-21 06:04:53 +0000

    Terminado caso A

---
+++ Clase 3./Taller.java
@@ -0,0 +1,74 @@
+public class Taller {
+    public static void main(String[] args) {
+        double d1 = 42.0, d2 = 58.5, d3 = 37.2;
+        double t1 = 0.9, t2 = 1.4, t3 = 0.8;
+        double l1 = 5.1, l2 = 6.9, l3 = 4.3;
+        double precioLitro = 1.35;
+        double masaCargaKg = 1200;
+        double largo = 2.0, ancho = 1.2, alto = 1.1;
+        double p1 = 2.5, p2 = 3.0, p3 = 2.0;
+        double deprecPorKm = 0.08;
+        double volCamionM3 = 10.0;
+        double fCO2 = 2.68;
+        double galPorLitro = 0.264172;
+        double vmin = 30.0, vmax = 90.0;
+        double va = 40.0, vb = 80.0, ca = 0.05, cb = 0.09;
+        double a = -0.008, b = 0.08, c = 4.0;
+
+        
+
+       System.out.println(d1/t1);
+       System.out.println(d2/t2);
+       System.out.println(d3/t3);
+       double v1 = (46.67);
+       double v2 = (41.79);
+       double v3 = (46.50);
+       System.out.println((d1*v1+d2*v2+d3*v3)/(d1+d2+d3));
+       System.out.println(d1/l1);
+       System.out.println(d2/l2);
+       System.out.println(d3/l3);
+       double klm1 = (8.24);
+       double klm2 = (8.48);
+       double klm3 = (8.65);
+       System.out.println((d1+d2+d3)/(l1+l2+l3));
+       double kmltotal = (8.44);
+       System.out.println((l1+l2+l3)*precioLitro);
+       double comb = (22.00);
+       System.out.println((d1+d2+d3)*deprecPorKm);
+       double dep = (11.01);
+       System.out.println(p1+p2+p3);
+       double peajes = (7.50);
+       System.out.println(comb+dep+peajes);
+       double directo = (40.52);
+       System.out.println(directo/(d1+d2+d3));
+       double costokm = (0.29);
+       System.out.println(largo*ancho*alto);
+       double volumenM3 = (2.64);
+       System.out.println(masaCargaKg/volumenM3);
+       double densidad = (454.55);
+       System.out.println(volumenM3/volCamionM3);
+       double ocupacion = (0.264);
+       System.out.println((l1+l2+l3)*fCO2);
+       double co2total = (43.68);
+       
+
+
+
+        
+
+        
+
+
+        
+
+        
+
+
+
+
+
+
+        
+    }
+    
+}
