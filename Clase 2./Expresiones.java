94ef119d1459b082d189a3044794a55884f00c5d
Tiaa4n <sebastian.ventesar@campusucc.edu.co>
2025-09-27 18:18:33 +0000

    Listo

--- Clase 2./Expresiones.java
+++ Clase 2./Expresiones.java
@@ -1,8 +1,8 @@
 public class Expresiones {
 public static void main(String[]args){
- int A = 4;
- int B = 5;
- int C = 1;
+ double A = 4;
+ double B = 5;
+ double C = 1;
       System.out.println(B * A - Math.pow(B,2) / 4 * C);
 }
 } 
