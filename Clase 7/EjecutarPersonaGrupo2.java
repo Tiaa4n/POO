c74b4d7a5e0aeb43162cbcd48a964b5707d2b7f7
Tiaa4n <sebastian.ventesar@campusucc.edu.co>
2025-09-27 18:32:24 +0000

    Tarea hecha

--- Clase 7/EjecutarPersonaGrupo2.java
+++ Clase 7/EjecutarPersonaGrupo2.java
@@ -1,8 +1,7 @@
 public class EjecutarPersonaGrupo2 {
     public static void main(String[] args) {
         
-        //Creación del objeto
-        //El operador "new" me permite instanciar(crear) el objeto
+        
         Persona objPersona1 = new Persona(1006234, 
                                             "Juan Manuel", 
                                             "Quijano López", 
@@ -17,9 +16,19 @@ public class EjecutarPersonaGrupo2 {
                                             1.84, 
                                             65.0);
 
+        
         System.out.println(objPersona2.estudiar("Ingeniería de Sistemas"));
 
-        //Tarea: implementar los metodos get y set en la clase Persona
-        //dichos métodos utilizarlos en la clase EjecutarPersonaGrupo2
+       
+        System.out.println("Nombre de la persona 1: " + objPersona1.getNombres());
+        System.out.println("Edad de la persona 2: " + objPersona2.getEdad());
+
+        
+        objPersona1.setEdad(24); 
+        objPersona2.setPeso(68.5);
+
+        
+        System.out.println("Nueva edad persona 1: " + objPersona1.getEdad());
+        System.out.println("Nuevo peso persona 2: " + objPersona2.getPeso());
     }
 }
--- Clase 7/Persona.java
+++ Clase 7/Persona.java
@@ -1,44 +1,75 @@
 public class Persona {
-
-    //Atributos
-    private int identificacion;
+    
+    private int id;
     private String nombres;
     private String apellidos;
     private int edad;
     private double estatura;
     private double peso;
 
-    //Método constructor de la clase se reconoce porque tiene el mismo nombre de la clase
-    //El constructor de la clase me permite inicializar la clase
-    //la palabra reservada "this" se utiliza para autoreferenciar el atributo de la clase
-    public Persona(int identificacion, String nombres, String apellidos, int edad, double estatura, double peso){
-       this.identificacion = identificacion;
-       this.nombres = nombres;
-       this.apellidos = apellidos;
-       this.edad = edad;
-       this.estatura = estatura;
-       this.peso = peso;
-    }
-
-    //Métodos
-    public void caminar(){
-        System.out.println("Caminar");
-    }
-
-    public boolean jugar(){
-        return true;
-    }
-
-    public void comer(){
-        System.out.println("Comiendo");
-    }
-
-    public String estudiar(String carrera){
-        return "Usted está estudiando: " + carrera;
-    }
-
-    //Tarea consultar sobre el método toString.
     
+    public Persona(int id, String nombres, String apellidos, int edad, double estatura, double peso) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.estatura = estatura;
+        this.peso = peso;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    
+    public String estudiar(String carrera) {
+        return nombres + " " + apellidos + " está estudiando " + carrera;
+    }
 }
-    
 
